bdebce7edaf25e37049db64d9d1aa80a4e2ec508
huanyuan <3042752146@qq.com>
2020-06-01 15:24:52 +0800

    解决没有html头问题 获取完整html 但是仍然没有获取到html前面这种documenttype标签

--- content_script.js
+++ content_script.js
@@ -1,6 +1,8 @@
 	chrome.extension.onMessage.addListener(
         function(request, sender, sendResponse) {if (request.action === "copy")
-                sendResponse($(":root").html());
-            else
+                //sendResponse($(":root").html());
+            //sendResponse(document.getElementsByTagName('html')[0].outerHTML);
+            sendResponse(document.documentElement.outerHTML);
+        else
                 sendResponse("FUCK FAILED"); // snub them.
         });
